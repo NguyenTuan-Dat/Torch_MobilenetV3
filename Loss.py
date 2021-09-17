03a597863abe34a23afecbc6fa8f148ff06b190e
ntdat <bkdn.ntdat@gmail.com>
2021-09-17 09:44:48 +0700

    drop hat

--- Loss.py
+++ Loss.py
@@ -18,45 +18,85 @@ class FocalLoss(torch.nn.Module):
         loss = (1 - p) ** self.gamma * logp
         return loss.mean()
 
-    def convert_target_to_target_format(self, targets):
+    def convert_target_to_target_format(targets):
         glasses_target = torch.zeros(len(targets), dtype=torch.long).cuda(0)
         mask_target = torch.zeros(len(targets), dtype=torch.long).cuda(0)
-        hat_target = torch.zeros(len(targets), dtype=torch.long).cuda(0)
+        # hat_target = torch.zeros(len(targets), dtype=torch.long).cuda(0)
 
         for idx, target in enumerate(targets):
             if target == 0:
                 glasses_target[idx] = 1
                 mask_target[idx] = 0
-                hat_target[idx] = 0
+                # hat_target[idx] = 0
             elif target == 1:
                 glasses_target[idx] = 1
                 mask_target[idx] = 0
-                hat_target[idx] = 1
+                # hat_target[idx] = 1
             elif target == 2:
                 glasses_target[idx] = 1
                 mask_target[idx] = 1
-                hat_target[idx] = 0
+                # hat_target[idx] = 0
             elif target == 3:
                 glasses_target[idx] = 0
                 mask_target[idx] = 0
-                hat_target[idx] = 1
+                # hat_target[idx] = 1
             elif target == 4:
-                glasses_target[idx] = 0
+                glasses_target[idx] = 1
                 mask_target[idx] = 1
-                hat_target[idx] = 0
+                # hat_target[idx] = 0
             elif target == 5:
                 glasses_target[idx] = 1
                 mask_target[idx] = 1
-                hat_target[idx] = 1
+                # hat_target[idx] = 1
             elif target == 6:
                 glasses_target[idx] = 0
                 mask_target[idx] = 1
-                hat_target[idx] = 1
+                # hat_target[idx] = 1
             elif target == 7:
                 glasses_target[idx] = 0
                 mask_target[idx] = 0
-                hat_target[idx] = 0
-        return glasses_target, mask_target, hat_target
+                # hat_target[idx] = 0
+        return glasses_target, mask_target
+
+    # def convert_target_to_target_format(self, targets):
+    #     glasses_target = torch.zeros(len(targets), dtype=torch.long).cuda(0)
+    #     mask_target = torch.zeros(len(targets), dtype=torch.long).cuda(0)
+    #     hat_target = torch.zeros(len(targets), dtype=torch.long).cuda(0)
+    #
+    #     for idx, target in enumerate(targets):
+    #         if target == 0:
+    #             glasses_target[idx] = 1
+    #             mask_target[idx] = 0
+    #             hat_target[idx] = 0
+    #         elif target == 1:
+    #             glasses_target[idx] = 1
+    #             mask_target[idx] = 0
+    #             hat_target[idx] = 1
+    #         elif target == 2:
+    #             glasses_target[idx] = 1
+    #             mask_target[idx] = 1
+    #             hat_target[idx] = 0
+    #         elif target == 3:
+    #             glasses_target[idx] = 0
+    #             mask_target[idx] = 0
+    #             hat_target[idx] = 1
+    #         elif target == 4:
+    #             glasses_target[idx] = 0
+    #             mask_target[idx] = 1
+    #             hat_target[idx] = 0
+    #         elif target == 5:
+    #             glasses_target[idx] = 1
+    #             mask_target[idx] = 1
+    #             hat_target[idx] = 1
+    #         elif target == 6:
+    #             glasses_target[idx] = 0
+    #             mask_target[idx] = 1
+    #             hat_target[idx] = 1
+    #         elif target == 7:
+    #             glasses_target[idx] = 0
+    #             mask_target[idx] = 0
+    #             hat_target[idx] = 0
+    #     return glasses_target, mask_target, hat_target
 
 class FocalLoss_Singletask(torch.nn.Module):
     def __init__(self, gamma = 2, eps = 1e-7):
